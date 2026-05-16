c2ec65fa5f637c4c6bc130a5a8b7a8d8e7eb7059
Alex Dridakis <alexdrid13@gmail.com>
2026-05-16 19:43:41 +0300

    add home page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    children: [
+      {
+        path: '',
+        loadComponent: () => import('./pages/home/home').then((m) => m.Home),
+      },
+    ],
+  },
+];
